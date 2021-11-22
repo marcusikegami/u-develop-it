11dba89f501f6628caaae2c63a7829b84436d471
Marcus Ikegami <hikegami.6@gmail.com>
2021-11-22 16:44:21 -0700

    add CRUD routes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 const mysql = require('mysql2');
+const inputCheck = require('./utils/inputCheck');
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -21,16 +22,78 @@ app.get('/', (req, res) => {
         message: 'Hello World'
     });
 });
+app.get('/api/candidates', (req, res) => {
+    const sql = `SELECT * FROM candidates`
+    db.query(sql, (err, rows) => {
+        if(err) {
+            res.status(500).json({ error: err.message })
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
 
-// db.query(`SELECT * FROM candidates`, (err, rows) => {
-//     console.log(rows);
-// })
-// db.query(`SELECT * FROM candidates WHERE id = 1`, (err, rows) => {
-//     if(err) {
-//         console.log(err);
-//     }
-//     console.log(rows);
-// })
+app.get('/api/candidate/:id', (req, res) => {
+    const sql = `SELECT * FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+    db.query(sql, params, (err, row) => {
+        if(err) {
+            res.status(400).json({ erro: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: row
+        });
+    }) ;
+});
+
+app.delete('/api/candidate/:id', (req, res) => {
+    const sql = `DELETE FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.statusMessage(400).json({ error: res.message });
+        } else if (!result.affectedRows) {
+            res.json({
+                message: 'Candidate not found.'
+            });
+        } else {
+            res.json({
+                message: 'deleted',
+                changes: result.affectedRows,
+                id: req.params.id
+            });
+        }
+    });
+});
+
+app.post('/api/candidate', ({body}, res) => {
+    const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+    if (errors) {
+        res.status(400).json({ error: errors });
+        return;
+    }
+    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
+    VALUES (?,?,?)`;
+    const params = [body.first_name, body.last_name, body.industry_connected];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ message: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: result
+        });
+    })
+});
+ 
 
 // const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
 //             VALUES (?,?,?,?)`;
