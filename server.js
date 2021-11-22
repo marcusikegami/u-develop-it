c61edb053a836eed570c1fdbe8ab0edc2a3dbc2f
Marcus Ikegami <hikegami.6@gmail.com>
2021-11-22 15:46:49 -0700

    added db queries to alter the SQL

--- server.js
+++ server.js
@@ -1,15 +1,46 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require('mysql2');
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: '!Ciabatta#%.F3',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
 app.get('/', (req, res) => {
     res.json({
         message: 'Hello World'
     });
 });
+
+// db.query(`SELECT * FROM candidates`, (err, rows) => {
+//     console.log(rows);
+// })
+// db.query(`SELECT * FROM candidates WHERE id = 1`, (err, rows) => {
+//     if(err) {
+//         console.log(err);
+//     }
+//     console.log(rows);
+// })
+
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+//             VALUES (?,?,?,?)`;
+// const params = [1, 'Ronald', 'Firbank', 1];
+// db.query(sql, params, (err, result) => {
+//     if(err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// })
 
 app.use((req, res) => {
     res.status(404).end();
